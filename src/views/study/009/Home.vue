3ebe2a1d1d43027034a59111e08b0b8ae125b6e3
gordonyaogz <gordonyaogz@gmail.com>
2022-12-16 18:29:12 +0800

    09 组件-子传父

---
+++ src/views/study/009/Home.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<List @fn='changeHome'></List>
+		父组件： {{msg}}
+	</div>
+</template>
+
+<script setup>
+import List from './List.vue'
+let msg=ref(20)
+const changeHome = (n)=>{
+	console.log( n.value );
+	msg=n
+}
+</script>
+
+
+<!-- <script type="text/javascript">
+import List from '../components/List.vue'
+export default{
+	components:{
+		List
+	},
+	setup(){
+		let changeHome = ( n )=>{
+			console.log( n.value )
+		}	
+		return {
+			changeHome
+		}
+	}
+}
+</script>
+ -->
+<!-- <script type="text/javascript">
+import List from '../components/List.vue'
+export default{
+	data () {
+		return {
+			n:0
+		}
+	},
+	components:{
+		List
+	},
+	methods:{
+		changeHome( n ){
+			this.n = n;
+			console.log( n )
+		}
+	}
+}
+</script> -->
---
+++ src/views/study/009/List.vue
@@ -0,0 +1,54 @@
+<template>
+	<div> 
+		这是子组件 ==> {{ num }}
+		<button @click='changeNum'>按钮</button>
+	</div>
+</template>
+
+<script setup lang='ts'>
+let num = ref(200);
+
+const emit = defineEmits<{
+  (e: 'fn', id: number): void
+}>()
+
+const changeNum = ()=>{
+	emit('fn',num)
+}	
+</script>
+
+
+
+<!-- <script type="text/javascript">
+export default{
+	setup(props,{ emit }){	
+
+		let num = ref(100);
+		const changeNum = ()=>{
+			emit('fn',num)
+		}
+
+		return {
+			num,
+			changeNum
+		}
+		
+	}
+}
+</script> -->
+
+
+<!-- <script type="text/javascript">
+export default{
+	data () {
+		return {
+			num:10
+		}
+	},
+	methods:{
+		changeNum(){
+			this.$emit('fn',this.num)
+		}
+	}
+}
+</script> -->
